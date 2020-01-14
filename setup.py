23d646fb0d540cccfcddfaca9c1a7a6b621fa06b
Niek Baeten <niek@utomik.com>
2020-01-14 15:48:45 +0100

    install latest version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 	author_email = 'max@eventbrite.com',
 	packages = ['zendesk'],
 	include_package_data = True,
-	install_requires = ['httplib2', 'simplejson'],
+	install_requires = ['httplib2==0.15.0', 'simplejson'],
 	license='LICENSE.txt',
 	url = 'https://github.com/eventbrite/zendesk',
 	keywords = 'zendesk api helpdesk',
